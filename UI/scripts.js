531ea64e1de692e6dc96bd64c77fcf9175e4a7d5
Osipov Alexander <fusion96@mail.ru>
2016-03-06 00:37:43 +0300

    GLOBAL script update

--- UI/scripts.js
+++ UI/scripts.js
@@ -1,58 +1,116 @@
-var counter = 0;
+var id = 0;
+var username = "Sasha";
 function run(){
 	var appContainer = document.getElementsByClassName('main')[0];
 
-	appContainer.addEventListener('click', changeName);
-	appContainer.addEventListener('click', sendMessage);
-	appContainer.addEventListener('keydown', sendMessage);
-	appContainer.addEventListener('click', deleteMessage);
+	appContainer.addEventListener('click', delegateMessage);
+	appContainer.addEventListener('keydown', delegateMessage);
+}
+
+function delegateMessage(evtObj) {
+	if(evtObj.type === 'click' && evtObj.target.classList.contains('changename-button')) {
+		changeName(evtObj);
+	}
+	if((evtObj.type === 'click' && evtObj.target.classList.contains('todo-button')) || evtObj.keyCode == 13) {
+		sendMessage(evtObj);
+	}
 }
 
 
 function changeName() {
-    var input = document.getElementById('name');
+	var input = document.getElementById('name');
  
     var s = document.getElementsByClassName('myName');
  
     for (var i = 0; i < s.length; i++) {
-    	s[i] = input.value;
+    	s[i].innerHTML = input.value;
     }
+
+    username = input.value;
 }
 
-function sendMessage(evtObj){
-	if((evtObj.type === 'click' && evtObj.target.classList.contains('todo-button')) || evtObj.keyCode == 13) {
-		var todoText = document.getElementById('todoText');
-		var itemName = document.getElementById('name');
-		var divItem = document.createElement('li');
-		var divName = document.createElement('li');
-		var but1 = document.createElement('button');
-		var but2 = document.createElement('button');
-   
-		but1.classList.add('delBut');
-		but2.classList.add('redBut');
-		divItem.classList.add('item');
-		divName.classList.add('myName');
+function sendMessage() {
+	var todoText = document.getElementById('todoText');
+	var divItem = document.createElement('li');
+	var divName = document.createElement('li');
+	var div = document.createElement('div');
+	var but1 = document.createElement('button');
+	var but2 = document.createElement('button');
+	var input = document.createElement('input');
+   		
+	but1.classList.add('delBut');
+	but2.classList.add('redBut');
+	divItem.classList.add('item');
+	divName.classList.add('myName');
 
-		//divItem.setAttribute('id' , 'liId' + counter);
+	divItem.setAttribute('id', 'divId' + id);
+	div.setAttribute('id', 'textDiv' + id);
+	input.setAttribute('id', 'textIn' + id);
+	input.setAttribute('class', 'In');
 
-		divItem.appendChild(but1);
-		divItem.appendChild(but2);
-		divItem.appendChild(document.createTextNode(todoText.value));
-		divName.appendChild(document.createTextNode(itemName.value));
+	var s = new String(id);
+	div.innerHTML = todoText.value;
 
-		if(todoText.value != "") {
-			document.getElementById('list').appendChild(divName);
-			document.getElementById('list').appendChild(divItem);
+	but1.addEventListener('click', function(){
+		deleteMessage(s);
+	});
+
+	but2.addEventListener('click', function(){
+		changeMessage(s);
+	});
+
+	divItem.appendChild(but1);
+	divItem.appendChild(but2);
+	divItem.appendChild(div);
+	divItem.appendChild(input);
+	divName.appendChild(document.createTextNode(username));
+	input.hidden = true;
+	div.hidden = false;
+
+	if(todoText.value != "") {
+		document.getElementById('list').appendChild(divName);
+		document.getElementById('list').appendChild(divItem);
+	}
+
+	todoText.value = "";
+	id++;
+}
+
+
+function deleteMessage(id) {
+	var k = document.getElementById('divId' + id);
+
+	document.getElementById('divId' + id).innerHTML = 'Сообщение удалено...';
+	k.classList.remove('item');
+	k.classList.add('myDeletedMessage');
+}
+
+function changeMessage(id) {
+	var k = document.getElementById('divId' + id);
+	var div = document.getElementById('textDiv' + id);
+	var input = document.getElementById('textIn' + id);
+	input.value = div.innerHTML;
+	input.hidden = false;
+	div.hidden = true;
+
+	input.addEventListener('keydown', function(e) {
+		if(e.keyCode == 13) {
+			div.innerHTML = input.value;
+			input.hidden = true;
+			div.hidden = false;
 		}
-
-		todoText.value = "";
-		counter++;
-	}	
+	});		
 }
 
-function deleteMessage() {
+
+
+		
+
+
+
+
+
 	
-}
 
 
 
