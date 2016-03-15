28be874e737b0b7e6df6c0cb5e4409b11beeaa96
Osipov Alexander <fusion96@mail.ru>
2016-03-15 15:05:44 +0300

    Global update with more feature

--- UI/scripts.js
+++ UI/scripts.js
@@ -1,10 +1,12 @@
 var id = 0;
 var username = "Sasha";
+var taskList = [];
 function run(){
 	var appContainer = document.getElementsByClassName('main')[0];
 
 	appContainer.addEventListener('click', delegateMessage);
 	appContainer.addEventListener('keydown', delegateMessage);
+	//restore();	 
 }
 
 function delegateMessage(evtObj) {
@@ -17,15 +19,19 @@ function delegateMessage(evtObj) {
 }
 
 function changeName() {
-	var input = document.getElementById('name');
- 
-    /*var s = document.getElementsByClass('myName');
- 
-    for (var i = 0; i < s.length; i++) {
-    	s[i] = input.value;
-    }*/
+    var input = document.getElementById('name');
+    var but1 = document.getElementsByClassName('delBut');
+    var but2 = document.getElementsByClassName('redBut');
 
-    username = input.value;
+    for(var i = 0; i < but1.length; i++){
+        if(input.value!=username)
+        but1[i].hidden = true;
+    }
+     for(var i = 0; i < but2.length; i++){
+        if(input.value!=username)
+        but2[i].hidden = true;
+    }
+    username = input.value;         
 }
 
 function sendMessage() {
@@ -44,17 +50,21 @@ function sendMessage() {
 	time.classList.add('time');
 	divItem.classList.add('item');
 	textName.classList.add('myName');
-
-	time.setAttribute('id', 't');
+        
+    but1.setAttribute('id','del' + id);
+    but2.setAttribute('id','red' + id);
+    but2.setAttribute('title','Click to open \nDouble-click to close');
+	time.setAttribute('id', 't' + id);
 	divItem.setAttribute('id', 'divId' + id);
 	text.setAttribute('id', 'textDiv' + id);
 	input.setAttribute('id', 'textIn' + id);
 	input.setAttribute('class', 'In');
+	text.setAttribute('class', 'Txt');
 
 	var s = new String(id);
 	text.innerHTML = todoText.value;
 	textName.innerHTML = username;
-	time.textContent = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+	time.innerHTML = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
 
 	but1.addEventListener('click', function(){
 		deleteMessage(s);
@@ -64,8 +74,18 @@ function sendMessage() {
 		changeMessage(s);
 	});
 
-	divItem.appendChild(but1);
+	but2.addEventListener('dblclick', function(){
+		changeMessage2(s);
+	});
+
+	divItem.addEventListener('keydown', function(e){
+		if(e.keyCode == 27){
+			changeMessage3(s);
+		}
+	});
+
 	divItem.appendChild(but2);
+	divItem.appendChild(but1);
 	divItem.appendChild(text);
 	divItem.appendChild(input);
 	divName.appendChild(time);
@@ -79,16 +99,15 @@ function sendMessage() {
 		document.getElementById('list').appendChild(divItem);
 	}
 
-	/*if(username == "Sasha") {
-		but1.hidden = false;
-		but2.hidden = false;
-	} else {
-		but1.hidden = true;
-		but2.hidden = true;
-	}*/
-
 	todoText.value = "";
 	id++;
+	
+	taskList.push(divName);
+	taskList.push(divItem);
+	store(taskList);
+
+	var box = document.getElementById('Box');
+	box.scrollTop = 9999;
 }
 
 
@@ -104,17 +123,54 @@ function changeMessage(id) {
 	var k = document.getElementById('divId' + id);
 	var text = document.getElementById('textDiv' + id);
 	var input = document.getElementById('textIn' + id);
+	var but1 = document.getElementById('del' + id);
 	input.value = text.innerHTML;
 	input.hidden = false;
 	text.hidden = true;
+	but1.hidden = true;
 
 	input.addEventListener('keydown', function(e) {
 		if(e.keyCode == 13) {
 			text.innerHTML = input.value;
 			input.hidden = true;
 			text.hidden = false;
+			but1.hidden = false;
 		}
 	});		
+}
+
+function changeMessage2(id) {
+	var text = document.getElementById('textDiv' + id);
+	var input = document.getElementById('textIn' + id);
+	var but1 = document.getElementById('del' + id);
+	input.hidden = true;
+	text.hidden = false;
+	but1.hidden = false;
+}
+
+function changeMessage3(id) {
+	var text = document.getElementsByClassName('Txt');
+	var input = document.getElementsByClassName('In');
+	var but1 = document.getElementsByClassName('delBut');
+	for (var i = 0; i < text.length; i++) {
+		text[i].hidden = false;
+	}
+	for (var i = 0; i < input.length; i++) {
+		input[i].hidden = true;
+	}
+	for (var i = 0; i < but1.length; i++) {
+		but1[i].hidden = false;
+	}
+}
+
+function restore() {
+	var item = localStorage.getItem("TODOs taskList");
+	alert(item);
+	return item && JSON.parse(item);
+}
+
+function store(listToSave) {
+	localStorage.setItem("TODOs taskList", JSON.stringify(listToSave));
 }
 
 
